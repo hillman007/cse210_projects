c0903e639fcfb7268a767be853ceff8ba86f3ba3
hillman007 <hillman.45@live.ca>
2025-04-04 13:03:46 -0600

    week 5 prove

--- week05/Mindfulness/Program.cs
+++ week05/Mindfulness/Program.cs
@@ -4,13 +4,7 @@ class Program
 {
     static void Main(string[] args)
     {
-        Dictionary<int, Activity> activities = new Dictionary<int, Activity>
-        {
-            { 1, new BreathingActivity() },
-            { 2, new ReflectionActivity() },
-            { 3, new ListingActivity() }
-        };
-
+        
         while (true)
         {
             Console.Clear();
@@ -21,16 +15,24 @@ class Program
             Console.WriteLine("4. Exit");
 
             Console.Write("Choose an activity (1-4): ");
-            string choice = Console.ReadLine();
+            int choice = int.Parse(Console.ReadLine());
 
-            if (choice == "4")
+            if (choice == 1)
+            {
+                BreathingActivity breathingActivity = new BreathingActivity();
+            }
+            else if (choice == 2)
+            {
+                ReflectionActivity reflectionActivity = new ReflectionActivity();
+            }
+            else if (choice == 3)
+            {
+                ListingActivity listingActivity = new ListingActivity();
+            }
+            if (choice == 4)
             {
                 Console.WriteLine("Goodbye!");
                 break;
-            }
-            else if (activities.ContainsKey(int.Parse(choice)))
-            {
-                activities[int.Parse(choice)].StartActivity();
             }
             else
             {
